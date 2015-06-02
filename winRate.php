4ba4a1d57ac3326768dfb01d2410c6cc343eeee5
YANG CHEN <ychen012@qq.com>
2015-06-02 09:36:06 -0700

    added winRate.php

---
+++ winRate.php
@@ -0,0 +1,51 @@
+<?php
+
+  	/***************************************************************
+		Description: 	City data in JSON.
+		Developer:	Vishal Kurup
+	***************************************************************/
+	
+	$host = "localhost"; //Your database host server
+	$db = "boostsho_loldb"; //Your database name
+	$user = "boostsho_root"; //Your database user
+	$pass = "a81538218"; //Your password
+	
+	$connection = mysql_connect($host, $user, $pass);
+	
+	//Check to see if we can connect to the server
+	if(!$connection)
+	{
+		die("Database server connection failed.");	
+	}
+	else
+	{
+		//Attempt to select the database
+		$dbconnect = mysql_select_db($db, $connection);
+		
+		//Check to see if we could select the database
+		if(!$dbconnect)
+		{
+			die("Unable to connect to the specified database!");
+		}
+		else
+		{
+			$query = "SELECT * FROM cities";
+			$resultset = mysql_query($query, $connection);
+			
+			$records = array();
+			
+			//Loop through all our records and add them to our array
+			while($r = mysql_fetch_assoc($resultset))
+			{
+				$records[] = $r;		
+			}
+			
+			//Output the data as JSON
+			echo json_encode($records);
+		}
+		
+		
+	}
+	
+
+?>
